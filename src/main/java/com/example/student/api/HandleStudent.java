fadf3bf805ec99ccd4fddb36af8043fd96327e5d
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-07-17 18:30:54 +0530

    create DTO class

---
+++ src/main/java/com/example/student/api/HandleStudent.java
@@ -0,0 +1,4 @@
+package com.example.student.api;
+
+public class HandleStudent {
+}
---
+++ src/main/java/com/example/student/dto/Student.java
@@ -0,0 +1,71 @@
+package com.example.student.dto;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private String city;
+    private String email;
+    private int level;
+
+    public Student(int studentID, String name, String city, String email, int level) {
+        this.studentID = studentID;
+        this.name = name;
+        this.city = city;
+        this.email = email;
+        this.level = level;
+    }
+
+    public Student() {
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentID=" + studentID +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", email='" + email + '\'' +
+                ", level=" + level +
+                '}';
+    }
+}
